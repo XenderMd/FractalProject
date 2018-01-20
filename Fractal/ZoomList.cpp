151c9c1f1b77235d52aa977ea6f039da9e0e7576
XenderMd <denis.stavilamd@gmail.com>
2018-01-20 23:35:47 +0100

    Applied Zoom and Scaling functionality

--- Fractal/ZoomList.cpp
+++ Fractal/ZoomList.cpp
@@ -12,10 +12,21 @@ ZoomList::ZoomList(int width, int height): m_width(width), m_height(height)
 void ZoomList::add(const Zoom & zoom)
 {
 	zooms.push_back(zoom);
+	
+	m_xCenter += (zoom.x - m_width / 2)*m_scale;
+	m_yCenter += (zoom.y - m_height / 2)*m_scale;
+
+	m_scale *= zoom.scale;
+
+	std::cout << m_xCenter << ", " << m_yCenter << ", " << m_scale << std::endl;
 
 }
 
 std::pair<double, double> caveofprogramming::ZoomList::doZoom(int x, int y)
 {
-	return std::pair<double, double>(0,0);
+	double xFractal = (x - m_width / 2)*m_scale + m_xCenter;
+	
+	double yFractal = (y - m_height / 2)*m_scale + m_yCenter;
+	
+	return std::pair<double, double>(xFractal,yFractal);
 }
