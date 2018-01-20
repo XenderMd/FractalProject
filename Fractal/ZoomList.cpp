c2cb24496cd9afeca2a3ae805a6e2d10b8dc9cf3
XenderMd <denis.stavilamd@gmail.com>
2018-01-20 15:38:33 +0100

    Added Zoom And Zoomlist
    Added Zoom, ZoomList and a basic scaling mechanism

--- Fractal/ZoomList.cpp
+++ Fractal/ZoomList.cpp
@@ -1,15 +1,23 @@
 #include "stdafx.h"
 #include "ZoomList.h"
 
+
 using namespace caveofprogramming;
 
-ZoomList::ZoomList(int width, int height): m_width(width), m_height(width)
+
+ZoomList::ZoomList(int width, int height): m_width(width), m_height(height)
 {
 }
 
 void ZoomList::add(const Zoom & zoom)
 {
 	zooms.push_back(zoom);
+	m_xCenter += (zoom.x - m_width / 2)*m_scale;
+	m_yCenter += (zoom.y - m_height / 2)*m_scale;
+
+	m_scale *= zoom.scale;
+	std::cout << m_xCenter << ", " << m_yCenter << ", " << m_scale <<std::endl;
+
 }
 
 std::pair<double, double> caveofprogramming::ZoomList::doZoom(int x, int y)
